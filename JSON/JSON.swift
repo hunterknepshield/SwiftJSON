5e79aa4fb5625f7bd90a990f733b9439cf138652
Hunter Knepshield <theragingfatcat@gmail.com>
2016-11-28 17:56:52 -0500

    Equatable conformance for JSON. Adding some accessor properties to the struct.

--- JSON/JSON.swift
+++ JSON/JSON.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2016 Hunter Knepshield. All rights reserved.
 //
 
-struct JSON {
-	enum JSONType {
+public struct JSON {
+	enum JSONValue {
 		case String(Swift.String)
 		case Number(Swift.String)
 		case Object(members: [Swift.String: JSON])
@@ -16,9 +16,91 @@ struct JSON {
 		case Null
 	}
 	
-	private var type: JSONType
+	var value: JSONValue
 	
-	internal init(_ type: JSONType) {
-		self.type = type
+	public var array: [JSON]? {
+		get {
+			switch self.value {
+			case .Array(let arr):
+				return arr
+			default:
+				return nil
+			}
+		}
+	}
+	public var string: String? {
+		get {
+			switch self.value {
+			case .String(let str):
+				return str
+			default:
+				return nil
+			}
+		}
+	}
+	public var bool: Bool? {
+		get {
+			switch self.value {
+			case .Boolean(let bool):
+				return bool
+			default:
+				return nil
+			}
+		}
+	}
+	
+	init(_ value: JSONValue) {
+		self.value = value
+	}
+	
+	public init?(string: String) {
+		let builder = Builder(json: string)
+		if let json = builder.build() {
+			self = json
+		} else {
+			return nil
+		}
 	}
 }
+
+extension JSON: Equatable {
+	public static func ==(lhs: JSON, rhs: JSON) -> Bool {
+		switch (lhs.value, rhs.value) {
+		case (.Null, .Null):
+			return true
+		case (.Boolean(let lb), .Boolean(let rb)):
+			return lb == rb
+		case (.String(let ls), .String(let rs)), (.Number(let ls), .Number(let rs)):
+			return ls == rs
+		case (.Array(let le), .Array(let re)):
+			if le.count != re.count {
+				return false
+			}
+			for i in 0..<le.count {
+				if le[i] != re[i] {
+					return false
+				}
+			}
+			return true
+		case (.Object(let lm), .Object(let rm)):
+			if lm.count != rm.count {
+				return false
+			}
+			for (lkey, lvalue) in lm {
+				guard let rvalue = rm[lkey] else {
+					return false
+				}
+				if lvalue != rvalue {
+					return false
+				}
+			}
+			return true
+		default:
+			return false
+		}
+	}
+}
+
+// TODO: investigate possibility of overriding "as?" casts
+// Syntax like this would be very useful:
+// if let s = json as? String { /* do work */ }
