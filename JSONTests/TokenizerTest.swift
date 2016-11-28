aa14bcd994174768712adf853e6fb3162714456b
Hunter Knepshield <theragingfatcat@gmail.com>
2016-11-28 17:35:54 -0500

    Added some invalid tokenization unit tests.

--- JSONTests/TokenizerTest.swift
+++ JSONTests/TokenizerTest.swift
@@ -21,7 +21,7 @@ class TokenizerTest: XCTestCase {
 				XCTAssertEqual(expected, actual)
 			}
 		} catch {
-			XCTFail("Error thrown while attempting to tokenize \(json)"	)
+			XCTFail("Error thrown while attempting to tokenize \(json)")
 		}
 	}
 	
@@ -63,9 +63,41 @@ class TokenizerTest: XCTestCase {
 	}
 	
 	func testMultipleTokenization() {
+		tokenizerHelper("false null", [.Boolean(false), .Null])
 		tokenizerHelper("{123", [.OpenObject, .Number("123")])
 		tokenizerHelper("123}", [.Number("123"), .CloseObject])
 		tokenizerHelper("{123}", [.OpenObject, .Number("123"), .CloseObject])
-		tokenizerHelper("[null", [.OpenArray, .Null])
+		tokenizerHelper("null[", [.Null, .OpenArray])
+		tokenizerHelper("null:true,false:\"false\"", [.Null, .Colon, .Boolean(true), .Comma, .Boolean(false), .Colon, .String("false")])
+	}
+	
+	/// Tokenizes the string up to the expected amount of tokens, then asserts
+	/// that attempting to call next() again will throw an error.
+	func badTokenizerHelper(_ json: String, _ tokens: [Token]) {
+		let tokenizer = Tokenizer(json: json)
+		do {
+			for expected in tokens {
+				guard let actual = try tokenizer.next() else {
+					XCTFail("Failed to produce more tokens while attempting to tokenize \(json)")
+					return
+				}
+				XCTAssertEqual(expected, actual)
+			}
+		} catch {
+			XCTFail("Threw too early while attempting to tokenize \(json)")
+		}
+		do {
+			_ = try tokenizer.next()
+			XCTFail("Did not throw at expected time while attempting to tokenize \(json)")
+		} catch {
+			// This is what we want.
+		}
+	}
+	
+	func testInvalidTokenization() {
+		badTokenizerHelper("nullfalse", [.Null])
+		badTokenizerHelper("false123", [.Boolean(false)])
+		badTokenizerHelper("fals", [])
+		badTokenizerHelper("{null\"123\"", [.OpenObject, .Null])
 	}
 }
