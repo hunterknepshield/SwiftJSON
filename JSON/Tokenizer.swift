de59511109e16b954bf6c8125e536e713978f7bf
Hunter Knepshield <theragingfatcat@gmail.com>
2016-11-28 12:58:56 -0500

    Tokenize the easy things - structural characters.

--- JSON/Tokenizer.swift
+++ JSON/Tokenizer.swift
@@ -9,13 +9,49 @@
 class Tokenizer {
 	private let json: String
 	private var iterator: String.CharacterView.Iterator
-	
+	/// Whether we've reached the end of the string or not.
+	private(set) var done = false
+	/// Used to find malformed JSON missing delimiters between tokens.
+	private var hasPriorSeparator = true
+
 	init(json: String) {
 		self.json = json
 		self.iterator = self.json.characters.makeIterator()
 	}
 	
 	func next() throws -> Token? {
+		if done {
+			return nil
+		}
+		while let c = iterator.next() {
+			switch c {
+			case " ", "\t", "\r", "\n":
+				// Just eat whitespace.
+				hasPriorSeparator = true
+			case ":":
+				hasPriorSeparator = true
+				return .Colon
+			case ",":
+				hasPriorSeparator = true
+				return .Comma
+			case "{":
+				hasPriorSeparator = true
+				return .OpenObject
+			case "}":
+				hasPriorSeparator = true
+				return .CloseObject
+			case "[":
+				hasPriorSeparator = true
+				return .OpenArray
+			case "]":
+				hasPriorSeparator = true
+				return .CloseArray
+			// TODO: null/true/false literals, strings, numbers
+			default:  // Unexpected character.
+				throw JSONError.Malformed
+			}
+		}
+		done = true
 		return nil
 	}
 }
