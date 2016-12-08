f890d9dac003e30e5ae9bb32deafbb25665d2421
Hunter Knepshield <theragingfatcat@gmail.com>
2016-12-07 22:22:24 -0500

    Added JSONDecodable and JSONEncodable protocols. Decodable has some nice default implementations. Next will be default implementations for Encodable.

---
+++ JSON/JSONDecodable.swift
@@ -0,0 +1,65 @@
+//
+//  JSONDecodable.swift
+//  JSON
+//
+//  Created by Hunter Knepshield on 12/7/16.
+//  Copyright © 2016 Hunter Knepshield. All rights reserved.
+//
+
+/// Conform to this protocol to get an initializer that constructs a conforming
+/// type from a JSON object. A convenience initializer allows a conforming type
+/// to be constructed directly from a raw JSON string.
+public protocol JSONDecodable {
+	/// Initialize an instance from a JSON object. Returns nil if the object
+	/// could not be initialized from the specified JSON.
+	init?(json: JSON)
+	
+	/// Attempts to construct a JSON object from the supplied string and uses
+	/// `init?(json:)` from there. Returns nil if a JSON object cannot be
+	/// constructed from the supplied string or if the user-defined initializer
+	/// returns nil.
+	init?(rawJson: String)
+}
+
+extension JSONDecodable {
+	init?(rawJson: String) {
+		guard let json = JSON(string: rawJson) else {
+			return nil
+		}
+		self.init(json: json)
+	}
+}
+
+extension Array where Element: JSONDecodable {
+	/// Attempts to construct an Array from a JSON array. Returns nil if any of
+	/// the elements of the JSON array fail to initialize an element object.
+	init?(jsonArray: [JSON]) {
+		self.init()
+		self.reserveCapacity(jsonArray.count)
+		for jsonElement in jsonArray {
+			guard let element = Element(json: jsonElement) else {
+				return nil
+			}
+			self.append(element)
+		}
+	}
+	
+	/// Attempts to construct an Array from a JSON object. Returns nil if the
+	/// JSON is not an array or if `init?(jsonArray:)` fails.
+	init?(json: JSON) {
+		guard let array = json.array else {
+			return nil
+		}
+		self.init(jsonArray: array)
+	}
+	
+	/// Attempts to construct an Array from a raw JSON array string. Returns nil
+	/// if a JSON object cannot be constructed from the supplied string or if
+	/// `init?(json:)` fails.
+	init?(rawJson: String) {
+		guard let json = JSON(string: rawJson) else {
+			return nil
+		}
+		self.init(json: json)
+	}
+}
---
+++ JSON/JSONEncodable.swift
@@ -0,0 +1,11 @@
+//
+//  JSONEncodable.swift
+//  JSON
+//
+//  Created by Hunter Knepshield on 12/7/16.
+//  Copyright © 2016 Hunter Knepshield. All rights reserved.
+//
+
+public protocol JSONEncodable {
+	var json: JSON? { get }
+}
